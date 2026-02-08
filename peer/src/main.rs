102236721213c76d6a233b7301a0ac90b601359f
Venom_9999 <140396453+VeNoM09999@users.noreply.github.com>
2026-02-08 22:31:22 +0530

    Working asking logic from relay as host and peer

--- peer/src/main.rs
+++ peer/src/main.rs
@@ -1,30 +1,38 @@
+#![allow(dead_code, unused)]
 use anyhow::Result;
+use clap::Parser;
+use eventsource_stream::{Event, Eventsource};
 use serde::{Deserialize, Serialize};
 use tokio as Runtime;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct PeerInfo {
-    connected_peer: String,
+#[derive(Debug, Clone, clap::ValueEnum)]
+enum Mode {
+    Host,
+    Peer,
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    url: String,
+    #[arg(long, value_enum)]
+    mode: Mode,
 }
 
 #[Runtime::main]
-async fn main() -> Result<()>{
-    println!("Peer booting up");
-    // let udp_socket = tokio::net::UdpSocket::bind("0.0.0.0:0").await;
-    let info = register_with_server("http://localhost:8080").await?;
-
-    println!("{:?}",info);
+async fn main() -> Result<()> {
+    let args = Args::parse();
+    match args.mode {
+        Mode::Host => {
+            println!("App started as host mode");
+            listen_to_relay(&args.url).await;
+        }
+        Mode::Peer => {
+            println!("App started as peer mode");
+            request_host_addr(&args.url).await;
+        }
+    }
     Ok(())
-}
-
-async fn register_with_server(server_url: &str) -> Result<PeerInfo> {
-    let client = reqwest::Client::new()
-        .get(server_url)
-        .body("Give me peer info!")
-        .send()
-        .await?;
-    let parsed_info = PeerInfo::from_response(client).await;
-    parsed_info
 }
 
 #[async_trait::async_trait]
@@ -34,13 +42,31 @@ trait FromResponse<T> {
         Self: Sized;
 }
 
-#[async_trait::async_trait]
-impl FromResponse<reqwest::Response> for PeerInfo {
-    async fn from_response(resp: reqwest::Response) -> Result<Self>
-    where
-        Self: Sized,
-    {
-        Ok(resp.json::<PeerInfo>().await?)
+use futures_util::StreamExt;
+async fn listen_to_relay(url: &str) -> Result<()> {
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        .header("Accept", "text/event-stream")
+        .send()
+        .await?;
+
+    println!("Connected to relay, waiting for peers...");
+
+    let mut stream = response.bytes_stream().eventsource();
+
+    while let Some(event) = stream.next().await {
+        println!("{:?}", event);
     }
+
+    Ok(())
 }
 
+async fn request_host_addr(url: &str) -> Result<()> {
+    let response = reqwest::Client::new().get(url).send().await;
+    if let Ok(r) = response {
+        println!("{}", r.text().await.unwrap());
+        // dbg!(r);
+    };
+    Ok(())
+}
