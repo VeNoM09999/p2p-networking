78ab67312fb6682b1d7f52760773b8dca0dbfc31
Venom_9999 <140396453+VeNoM09999@users.noreply.github.com>
2026-06-01 22:30:33 +0530

    feat: Update main application for sharded io_uring with gRPC

--- turn_server/src/main.rs
+++ turn_server/src/main.rs
@@ -1,22 +1,19 @@
+use common::relay::relay_control_server::RelayControlServer;
 use heapless::spsc::Queue;
 use std::os::fd::AsRawFd;
+use tokio::sync::mpsc;
+use tonic::transport::Server;
 use tracing::Level;
 use tracing::event;
 use turn_server::network::BufferPool;
-use turn_server::network::Session;
+use turn_server::network::MessageType;
+use turn_server::network::RelayControlService;
 use turn_server::network::io_uring_loop;
-use turn_server::network::{
-    BUFFER_SIZE, NUM_SHARDS, POOL_SIZE, Packet, Shard, ShardRouter, SqeType,
-};
+use turn_server::network::{BUFFER_SIZE, POOL_SIZE, Shard, ShardRouter};
 
 // ============================================================
 // MAIN
 // ============================================================
-
-static mut SHARD_QUEUES: [Queue<Packet, BUFFER_SIZE>; NUM_SHARDS] =
-    [const { Queue::new() }; NUM_SHARDS];
-static mut ROUTER_QUEUES: [Queue<SqeType, BUFFER_SIZE>; NUM_SHARDS] =
-    [const { Queue::new() }; NUM_SHARDS];
 
 fn init_tracing() {
     use std::fs::File;
@@ -44,24 +41,30 @@ fn init_tracing() {
         )
         .init();
 }
+const SHARD_NUM: usize = 2;
+static mut SHARD_QUEUES: [Queue<MessageType, BUFFER_SIZE>; SHARD_NUM] = [const { Queue::new() }; 2];
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // let relay_service =
+    let cores = core_affinity::get_core_ids().expect("Failed to get the cores id ");
+
     init_tracing();
     event!(Level::INFO, "Starting TURN server");
     // Build shard channels
-    let mut shard_prods = Vec::with_capacity(NUM_SHARDS);
-    let mut shards: Vec<Shard> = Vec::with_capacity(NUM_SHARDS);
-    let mut ring_consumers = Vec::with_capacity(NUM_SHARDS);
+    let mut shard_prods = Vec::with_capacity(SHARD_NUM);
+    let (tx, rx) = mpsc::channel(1024 * SHARD_NUM);
 
-    for id in 0..NUM_SHARDS {
-        let shards_ref = unsafe { &mut *(&raw mut SHARD_QUEUES) };
-        let shardrouter_ref = unsafe { &mut *(&raw mut ROUTER_QUEUES) };
-        let (ring_producer, ring_consumer) = shards_ref[id].split();
-        let (shard_producer, shard_consumer) = shardrouter_ref[id].split();
-        ring_consumers.push(shard_consumer);
+    for id in 0..SHARD_NUM {
+        let core = cores[id];
+        let cloned = tx.clone();
+        let shard_ref = unsafe { &mut *(&raw mut SHARD_QUEUES) };
+        let (ring_producer, ring_consumer) = shard_ref[id].split_const();
         shard_prods.push(ring_producer);
-        shards.push(Shard::new(id, ring_consumer, shard_producer));
+        std::thread::spawn(move || {
+            core_affinity::set_for_current(core);
+            let mut shard = Shard::new(id, ring_consumer, cloned.clone());
+            shard.run();
+        });
     }
 
     let socket = std::net::UdpSocket::bind("0.0.0.0:8080").expect("Failed to bind port");
@@ -76,23 +79,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build(POOL_SIZE as u32)?;
     // let mut io_uring = io_uring::IoUring::new(2048).expect("Failed to set ioring");
 
-    for mut shard in shards {
-        std::thread::spawn(move || {
-            if shard.id == 0 {
-                shard.add_session(Session {
-                    host_addr: "127.0.0.1:9001".parse().unwrap(),
-                    peer_addr: "127.0.0.1:9002".parse().unwrap(),
-                });
-            }
-            shard.run();
+    let (control_tx, control_rx) = mpsc::channel(1024);
+
+    let grpc_service = RelayControlService { control_tx };
+
+    std::thread::spawn(|| {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(async {
+            event!(Level::INFO, "gRPC service started");
+
+            Server::builder()
+                .add_service(RelayControlServer::new(grpc_service))
+                .serve("0.0.0.0:50051".parse().unwrap())
+                .await
+                .unwrap()
         });
-    }
+    });
+
+    core_affinity::set_for_current(cores[cores.len() - 1]);
     io_uring_loop(
         &mut io_uring,
-        ring_consumers,
+        rx,
         &mut buffer_pool,
         &mut shard_router,
         fd,
+        control_rx,
     );
 
     Ok(())
